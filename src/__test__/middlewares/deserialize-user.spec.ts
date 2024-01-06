622cc8b54169911b5fef122ef2f4e4ffd281314d
Mohamed Aljoke <mohamed@idip.com.br>
2024-01-05 23:05:56 -0300

    feat: add unit testing

--- src/__test__/middlewares/deserialize-user.spec.ts
+++ src/__test__/middlewares/deserialize-user.spec.ts
@@ -1,7 +1,19 @@
 import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
-import { verifyToken } from '@/utils/jwt.utils';
+import * as jwt from '@/utils/jwt.utils';
+import * as envValidator from '@/validation/env.validator';
 import { deserializeUser } from '@/middlewares/deserialize-user';
 
+vi.mock('../../validation/env.validator', () => ({
+  validatedEnv: {
+    JWT_ACCESS_TOKEN_HASH_KEY: 'JWT_ACCESS_TOKEN_HASH_KEY',
+  },
+}));
+vi.mock('../validation/env.validator', () => {
+  const validatedEnv = {
+    JWT_ACCESS_TOKEN_HASH_KEY: 'JWT_ACCESS_TOKEN_HASH_KEY',
+  };
+  return { validatedEnv };
+});
 describe('require-user', () => {
   let req: any;
   let res: any;
@@ -25,19 +37,19 @@ describe('require-user', () => {
     req.cookies.access_token = 'validAccessToken';
     const decodedUser = { id: 1, username: 'user123' };
     const validTokenResult = { decoded: decodedUser, expired: false };
-    vi.mock('../validation/env.validator', () => {
-      const validatedEnv = {
-        JWT_ACCESS_TOKEN_HASH_KEY: 'JWT_ACCESS_TOKEN_HASH_KEY',
-      };
-      return { validatedEnv };
-    });
-    vi.mock('../utils/jwt.utils', () => {
-      const verifyToken = vi.fn();
-      verifyToken.mockReturnValue(validTokenResult);
-      return {
-        verifyToken,
-      };
-    });
-    // deserializeUser(req, res, next);
+
+    vi.spyOn(jwt, 'verifyToken').mockReturnValue(validTokenResult);
+
+    deserializeUser(req, res, next);
+    expect(res.locals.user).toEqual(decodedUser);
+  });
+  it('Should not set user if there is no token', () => {
+    const decodedUser = { id: 1, username: 'user123' };
+    const validTokenResult = { decoded: decodedUser, expired: false };
+
+    vi.spyOn(jwt, 'verifyToken').mockReturnValue(validTokenResult);
+
+    deserializeUser(req, res, next);
+    expect(res.locals.user).toEqual(undefined);
   });
 });
